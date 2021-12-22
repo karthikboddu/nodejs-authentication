7a3a440b3f851da077fded74acdac10586537e6d
karthikboddu <karthikb5566@gmail.com>
2021-12-22 22:47:12 +0530

    start mongo

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://karthikboddu:Kartik@23@cluster0.kkjuj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/sample_nodejs_angular11db"
+  url: "mongodb+srv://karthikboddu:Kartik@23@cluster0.kkjuj.mongodb.net/myFirstDatabase?retryWrites=true/sample_nodejs_angular11db"
 };
