272d6a2a6a0c1e9c13051b43c053c0eb8e55b047
karthikboddu <karthikb5566@gmail.com>
2021-12-22 22:27:23 +0530

    redis

--- app/helpers/init_redis.js
+++ app/helpers/init_redis.js
@@ -1,7 +1,7 @@
 const redis = require("redis");
 
 const redisCli = redis.createClient({
-  url: "redis://:p7e45383a68e81a1c10f95227c419c81ff8615c59c8b235f858ea965aac28a096@ec2-52-203-53-204.compute-1.amazonaws.com:20230",
+  url: REDIS_URL,
   socket: {
     tls: true,
     rejectUnauthorized: false
