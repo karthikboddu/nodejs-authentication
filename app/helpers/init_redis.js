78e80729eb398494364ea4f8f44df6275fa84c0b
karthikboddu <karthikb5566@gmail.com>
2021-12-22 22:11:00 +0530

    heruko redis

--- app/helpers/init_redis.js
+++ app/helpers/init_redis.js
@@ -1,8 +1,8 @@
 const redis = require("redis");
 
 const redisCli = redis.createClient({
-    port:20229,
-    host:"p7e45383a68e81a1c10f95227c419c81ff8615c59c8b235f858ea965aac28a096@ec2-52-203-53-204.compute-1.amazonaws.com"
+    port:20230,
+    host:"ec2-52-203-53-204.compute-1.amazonaws.com"
 });
 
 redisCli.on("connect", function () {
