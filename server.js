8c3a49189c2f731b3bdbae8370d95183404eb7f1
karthikboddu <karthikb5566@gmail.com>
2021-12-24 00:04:37 +0530

    socket

--- server.js
+++ server.js
@@ -13,6 +13,43 @@ require('./app/helpers/init_redis');
 var a = require('crypto').randomBytes(64).toString('hex');
 
 
+
+// A Node.js server
+var server = require('http').Server();
+
+// Requiring the ioredis package
+var Redis = require('ioredis');
+// A redis client
+var redis = new Redis();
+var io = require('socket.io')(server);
+// Store people in chatroom
+var chatters = [];
+
+// Store messages in chatroom
+var chat_messages = [];
+
+// Subscribe to all channels which name complies with the '*' pattern
+// '*' means we'll subscribe to ALL possible channels
+redis.psubscribe('*');
+
+// Listen for new messages
+redis.on('pmessage', function (pattern, channel, message) {
+    message = JSON.parse(message);
+	io.emit(""+message.data.data.user_id+message.data.data.receiver_id, message.data);
+    // Just to check that things really work
+    if(channel == "laravel_database_userjoined App\Events\MessageSent"){
+    	io.emit('user-joined',message );
+    }
+    
+	const mdata = message.data;
+    console.log(channel, message.event,message.data,pattern);
+});
+
+
+
+
+
+
 var corsOptions = {
  origin : "*"
 }
