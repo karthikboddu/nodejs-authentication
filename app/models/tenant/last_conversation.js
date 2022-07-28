327882e52c84a36dcaa66e29ff22b93e30268552
karthikboddu <karthikb5566@gmail.com>
2022-07-28 15:43:02 +0530

    sockets

---
+++ app/models/tenant/last_conversation.js
@@ -0,0 +1,35 @@
+module.exports = (mongoose) => {
+
+    const lastConversation = mongoose.model("last_conversation",
+                                new mongoose.Schema({
+                                    recipients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'tenant' }],
+                                    lastMessage: {
+                                        type: String,
+                                    },
+                                    parent_id :{
+                                        type : mongoose.Schema.Types.ObjectId,
+                                        required: true,
+                                        ref: "tenant"
+                                    },
+                                    seen: {
+                                        type: Boolean,
+                                        default :false
+                                    },                         
+                                    is_active: {
+                                        type: Boolean,
+                                        default :true
+                                    },
+                                    created_at: {
+                                        type : Date,
+                                        default : Date.now,
+                                    },
+                                    updated_at: {
+                                        type : Date,
+                                        default : Date.now,
+                                    }
+
+    }))
+
+return lastConversation;
+
+}
